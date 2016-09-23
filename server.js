9f09ad0864727c053c80aca99023d5e801e8c55c
Ashique17 <siddique.ashique@gmail.com>
2016-09-23 23:37:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var tests={
+test1 :{
+    title:"Test One | Ashique",
+    heading:"Test One",
+    content:"<p>Test One Sucessfully Completed!!!</p>"
+},
+test2:{
+    title:"Test Two | Ashique",
+    heading:"Test Two",
+    content:"<p>Test Two Sucessfully Completed!!!</p>"
+},
+test3:{
+    title:"Test Three | Ashique",
+    heading:"Test Three",
+    content:"<p>Test Three Sucessfully Completed!!!</p>"
+}
+};
+function createhtml(data){
+    var title= data.title;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate=`
+<html>
+    <head>
+        <title> ${title} </title>
+        <meta name="viewport" content="width-device-width, initial-scale= 1"/>
+    </head>
+    <body>
+        <div>
+            <a href='/'>Home</a>
+        </div>
+        <div>
+            <h1>
+            ${heading}
+            </h1>
+        </div>
+        <div>
+            <p>
+                ${content}
+            </p>
+        </div>
+    </body>
+</html>`;
+return createhtml;
+}
+    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'test1.html'));
+app.get('/:testname', function (req, res) {
+   var testname=req.params.testname;
+  res.sendFile(createhtml(tests[testname]));
 });
 
-app.get('/test2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'test2.html'));
-});
-
-app.get('/test3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'test3.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
