a06429dc289f4be750d01743685475ca782047b0
Ashique17 <siddique.ashique@gmail.com>
2016-09-23 22:14:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test1', function (req, res) {
+  res.send('URL Sucessfully Added for test1');
+});
+
+app.get('/test2', function (req, res) {
+  res.send('URL Sucessfully Added for test2');
+});
+
+app.get('/test3', function (req, res) {
+  res.send('URL Sucessfully Added for test3');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
