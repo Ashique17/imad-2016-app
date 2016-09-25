b6e8f043b83a64d7fb734e1e9ebbd5a26a55f7db
Ashique17 <siddique.ashique@gmail.com>
2016-09-25 21:35:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('txt');
 element.innerHTML = 'New Text';
+
+var img=document.getElementByID('img');
+img.onclick = function(){
+    img.style.marginLeft = '200px';
+};
